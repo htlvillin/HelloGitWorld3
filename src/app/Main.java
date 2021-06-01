40522f08e2f5536d79b2f0a503eddc94a61ef840
Niksaas <pliessnn@edu.htl-villach.at>
2021-06-01 08:17:54 +0200

    Another new Greeting

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Git-Hub World!");
+        System.out.println("Hello Git-Hub World");
+        System.out.println("Guten Morgen");
     }
 }
